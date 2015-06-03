6a41ea5907777938e86aaa7d2e7751824e93fb8e
serge <markovs@programpark.ru>
2015-06-03 12:23:12 +0300

    Добавлен фолдинг импортов в стиле Ruby - в любом месте файла, а также фолдинг Java и Ruby (многострочных) комментариев и JavaDoc.

--- mirah-lang/src/main/java/ca/weblite/netbeans/mirah/MirahStructureAnalyzer.java
+++ mirah-lang/src/main/java/ca/weblite/netbeans/mirah/MirahStructureAnalyzer.java
@@ -1,11 +1,14 @@
 package ca.weblite.netbeans.mirah;
 
 import ca.weblite.netbeans.mirah.lexer.Block;
+import ca.weblite.netbeans.mirah.lexer.DocumentQuery;
 import ca.weblite.netbeans.mirah.lexer.MirahParser.NBMirahParserResult;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.swing.text.Document;
+
 import org.netbeans.modules.csl.api.ElementKind;
 import org.netbeans.modules.csl.api.OffsetRange;
 import org.netbeans.modules.csl.api.StructureItem;
@@ -30,25 +33,116 @@ public class MirahStructureAnalyzer implements StructureScanner {
         return out;
     }
 
+    /**
+     * Compute a list of foldable regions, named "codeblocks", "comments", "imports", "initial-comment", ... The
+     * returned Map must be keyed by {@link FoldType#code}. For backward compatibility, the following tokens are
+     * temporarily supported although no FoldType is registered explicitly.
+     * <ul>
+     * <li>codeblocks
+     * <li>comments
+     * <li>initial-comment
+     * <li>imports
+     * <li>tags
+     * <li>inner-classes
+     * <li>othercodeblocks
+     * </ul>
+     * This additional support will cease to exist after NB-8.0. Language owners are required to register their
+     * {@link FoldTypeProvider} and define their own folding.
+     */
     @Override
     public Map<String, List<OffsetRange>> folds(ParserResult pr) {
         Map<String, List<OffsetRange>> out = new HashMap<>();
         NBMirahParserResult res = (NBMirahParserResult) pr;
-        ArrayList<OffsetRange> ranges = new ArrayList<>();
+        // узел PACKAGE почему-то сделан родительским узлом всех блоков
         for (Block block : res.getBlocks()) {
-            LOG.info(this, "block = " + block);
-            // todo узел PACKAGE почему-то сделан родительским узлом всех блоков. из-за этого не работает фолдинг. 
-            // Либо исправить размер, чтобы он охватывал все подчиненные узлы, либо убрать из списка блоков. разобраться
-            if (block.getKind() == ElementKind.PACKAGE) {
-                for (Block child : block.getChildren()) {
-                    ranges.add(new OffsetRange(child.getOffset(), child.getOffset() + child.getLength()));
-                }
-            } else {
-                ranges.add(new OffsetRange(block.getOffset(), block.getOffset() + block.getLength()));
+            folds(block, null, out, pr);
+        }
+        for (Block block : res.getImports()) {
+            folds(block, null, out, pr);
+        }
+        for (Block block : res.getLineComments()) {
+            folds(block, null, out, pr);
+        }
+        for (Block block : res.getBlockComments()) {
+            folds(block, null, out, pr);
+        }
+        return out;
+    }
+
+    /**
+     * Первую строку того, что заворачиваем, оставляем видимой. todo: методы и классы с аннотациями - первой строкой
+     * метода считается аннотация, которая может быть в отдельной строке.
+     *
+     * @param block
+     * @param parent приходится передавать также родителя, т.к. у потомка нет ссылки на родительский блок.
+     * @param out
+     * @return
+     */
+    private void folds(Block block, Block parent, Map<String, List<OffsetRange>> out, ParserResult pr) {
+        List<OffsetRange> ranges = getRanges(block, parent, out);
+        if (ranges != null) {
+            final Document doc = pr.getSnapshot().getSource().getDocument(false);
+            DocumentQuery dq = new DocumentQuery(doc);
+            int eol = dq.getEOL(block.getOffset());
+            if (eol <= block.getOffset() + block.getLength()) {
+                ranges.add(new OffsetRange(eol, block.getOffset() + block.getLength()));
             }
         }
-        out.put("codeblocks", ranges);
-        return out;
+        for (Block child : block.getChildren()) {
+            folds(child, block, out, pr);
+        }
+        for (Block child : block.getImports()) {
+            folds(child, block, out, pr);
+        }
+        // Фолдинг для блоков do end и { }
+        if (block.getKind() == ElementKind.METHOD) {
+            for (Block child : block.getBlocks()) {
+                folds(child, block, out, pr);
+            }
+        }
+    }
+
+    /**
+     *
+     * @param block
+     * @param parent приходится передавать также родителя, т.к. у потомка нет ссылки на родительский блок.
+     * @param out
+     * @return
+     */
+    private List<OffsetRange> getRanges(Block block, Block parent, Map<String, List<OffsetRange>> out) {
+        // Диапазоны сохраняем только для определенных типов блоков
+        switch (block.getKind()) {
+            case CLASS:
+            case INTERFACE:
+                if (parent.getKind() == ElementKind.PACKAGE) {
+                    // Классы верхнего уровня не фолдим, чтобы не перегружать интерфейс
+                    return null;
+                } else {
+                    return getRanges("inner-classes", out);
+                }
+            case CONSTRUCTOR:
+            case METHOD:
+                return getRanges("codeblocks", out);
+            case OTHER:
+                return getRanges("imports", out);
+            case TAG:
+                // Однострочные комментарии в стиле Ruby, начинающиеся с #
+                return getRanges("comments", out);
+            case DB:
+                // Комментарии в стиле Java, начинающиеся с /* и /**
+                return getRanges("initial-comment", out);
+            default:
+                return null;
+        }
+    }
+
+    private List<OffsetRange> getRanges(String what, Map<String, List<OffsetRange>> out) {
+        List<OffsetRange> ranges = out.get(what);
+        if (ranges == null) {
+            ranges = new ArrayList<>();
+            out.put(what, ranges);
+        }
+        return ranges;
     }
 
     @Override
